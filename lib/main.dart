f4e842b79a53f6c26ec790207fb19df21ddd8a62
Jade <jmorrison1@css.edu>
2023-05-09 13:35:19 -0500

    Changed text background and alignment Changed the Ken burn effect to allow artwork with different aspect rations to fit better Also made the audio work

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Gawbay Ojibwe',
+        title: '- Gawbay Ojibwe -',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
